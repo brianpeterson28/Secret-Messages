2be891a3aa3a13ead6317698200f538bee6a8408
Brian Peterson <brianpeterson28@gmail.com>
2017-12-09 20:48:29 -0600

    Display Logic
    Created main logic loop with supporting helper methods to (1) display
    cipher options, (2) get user input, (3) show that data was getting
    received and returned properly.

---
+++ secret_messages.py
@@ -0,0 +1,50 @@
+import os
+
+AVAILABLE_CIPHERS = ["Caesar", "Affine", "Atbash", "Key Word"]
+
+def main():
+	show_welcome()
+	show_available_ciphers(AVAILABLE_CIPHERS)
+	cipher = get_cipher()
+	message = get_message()
+	print("The user selected the follwoing cipher: " + cipher)
+	print("The user entered the follwoing message: " + "\"" + message + "\"")
+	if end_program():
+		pass
+	else:
+		clear_screen()
+		main() 
+
+def show_welcome():
+	print("\nThis is the Secret Messages Project for the Treehouse" 
+		+ "Techdegree.")
+
+def show_available_ciphers(list_of_ciphers):
+	print("The following ciphers are available:\n")
+	num = 1
+	for item in list_of_ciphers:
+		print(str(num) + ". " + item)
+		num += 1
+	print("\n")
+
+def get_cipher():
+	cipher = input("Please enter the name of the cipher you would " +  
+		           "like to use. > ")
+	return cipher
+
+def get_message():
+	message = input("Please enter the message. > ")
+	return message
+
+def end_program():
+	end = input("Would you like to encrypt or decrypt another message? > ")
+	if end == "Yes":
+		return False
+	else:
+		return True
+
+def clear_screen():
+	os.system('cls' if os.name == 'nt' else 'clear') 
+
+if __name__ == '__main__':
+	main()
