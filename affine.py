443edf4439389ebda4a5f6f258f90e3cbbf3d152
Brian Peterson <brianpeterson28@gmail.com>
2017-12-11 21:20:50 -0600

    Initialize Affine Class
    Create two dictionaries that allow transformations necessary to encrypt
    message. Added test code to make sure initialization code is working as
    expected.

--- affine.py
+++ affine.py
@@ -1,4 +1,28 @@
 from ciphers import Cipher
+import string
 
 class Affine(Cipher):
-	pass
+
+	def __init__(self):
+		self.alphabet = string.ascii_uppercase
+		self.alpha_2_num = {}
+		self.num_2_alpha = {}
+		index = 0
+		for char in self.alphabet:
+			self.alpha_2_num[char] = index
+			self.num_2_alpha[index] = char
+			index += 1 
+
+	def encrypt(self, text):
+		pass
+
+	def decrypt(self, text):
+		pass
+
+
+if __name__ == '__main__':
+	affine = Affine()
+	print("\nThe Affine class's alphabet is: " + affine.alphabet + "\n")
+	print("The letter to number encoding is: " + str(affine.alpha_2_num)+"\n")
+	print("The number to letter encoding is: " + str(affine.num_2_alpha)+"\n")
+
