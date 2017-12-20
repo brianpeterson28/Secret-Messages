f5f3da2a1710ba3b891de1d930b85c410f48ce4e
Brian Peterson <brianpeterson28@gmail.com>
2017-12-20 07:06:42 -0600

    PEP 8 Edits
    Converted tabs to spaces for PEP 8 purposes.

--- Keyword.py
+++ Keyword.py
@@ -3,94 +3,94 @@ import copy
 import string
 
 class Keyword(Cipher):
-	def __init__(self):
-		"""Initializes the Keyword cipher class.
+    def __init__(self):
+        """Initializes the Keyword cipher class.
 
-		The Keyword cipher class initializes itself with the uppercase ASCII 
-		alphabet as the alphabet to be used when encrypting and decrypting 
-		messages.
-		"""
+        The Keyword cipher class initializes itself with the uppercase ASCII 
+        alphabet as the alphabet to be used when encrypting and decrypting 
+        messages.
+        """
 
-		self.normal_alphabet = []
-		for item in string.ascii_uppercase:
-			self.normal_alphabet.append(item)
-
-
-	def encrypt(self, text, keyword):
-		"""Encrypts messages using the Keyword algorithm.
-
-		text is the message to be encrypted. 
-		keyword is the keyword used to shift the alphabet. 
-		"""
-
-		text = text.upper()
-		encrypted_message = ""
-		self.create_kw_list(keyword)
-		self.keyword_alphabet = self.create_kw_alphabet()
-		self.encrypt_key = dict(zip(self.normal_alphabet, 
-									self.keyword_alphabet))
-		for letter in text:
-			try:
-				encrypted_message += self.encrypt_key[letter]
-			except KeyError:
-				encrypted_message += letter
-		return encrypted_message
+        self.normal_alphabet = []
+        for item in string.ascii_uppercase:
+            self.normal_alphabet.append(item)
 
 
-	def decrypt(self, text, keyword):
-		"""Decrypts messages encrypted by the Keyword algorithm. 
+    def encrypt(self, text, keyword):
+        """Encrypts messages using the Keyword algorithm.
 
-		text is the message to be decrypted.
-		keyword is the keyword that was originally used to encrypt the message. 
-		"""
+        text is the message to be encrypted. 
+        keyword is the keyword used to shift the alphabet. 
+        """
 
-		text = text.upper()
-		decrypted_message = ""
-		self.create_kw_list(keyword)
-		self.keyword_alphabet = self.create_kw_alphabet()
-		self.decrypt_key = dict(zip(self.keyword_alphabet, 
-									self.normal_alphabet))
-		for letter in text:
-			try:
-				decrypted_message += self.decrypt_key[letter]
-			except KeyError:
-				decrypted_message += letter
-		return decrypted_message
-
-
-	def create_kw_list(self, keyword):
-		"""Prepares keyword string for processing by encrypt() and decrypt().
-
-		This is a helper function  that converts keyword from a string to a 
-		list so that keyword can be utilized more readily by the encrypt and 
-		decrypt methods.
-		"""
-
-		keyword = keyword.upper()
-		self.keyword_list = []
-		for item in keyword:
-			self.keyword_list.append(item)
+        text = text.upper()
+        encrypted_message = ""
+        self.create_kw_list(keyword)
+        self.keyword_alphabet = self.create_kw_alphabet()
+        self.encrypt_key = dict(zip(self.normal_alphabet, 
+                                    self.keyword_alphabet))
+        for letter in text:
+            try:
+                encrypted_message += self.encrypt_key[letter]
+            except KeyError:
+                encrypted_message += letter
+        return encrypted_message
 
 
-	def create_kw_alphabet(self):
-		"""Creates translation key for encrypting and decrypting messages.
+    def decrypt(self, text, keyword):
+        """Decrypts messages encrypted by the Keyword algorithm. 
 
-		This is a helper function that creates the alphabet used to translate 
-		messages using the Keyword algorithm.
-		"""
+        text is the message to be decrypted.
+        keyword is the keyword that was originally used to encrypt the message. 
+        """
 
-		kw_alphabet = copy.deepcopy(self.normal_alphabet)
-		for item in self.keyword_list:
-			for letter in kw_alphabet:
-				if item == letter:
-					kw_alphabet.remove(letter)
-		kw_alphabet = self.keyword_list + kw_alphabet
-		return kw_alphabet
+        text = text.upper()
+        decrypted_message = ""
+        self.create_kw_list(keyword)
+        self.keyword_alphabet = self.create_kw_alphabet()
+        self.decrypt_key = dict(zip(self.keyword_alphabet, 
+                                    self.normal_alphabet))
+        for letter in text:
+            try:
+                decrypted_message += self.decrypt_key[letter]
+            except KeyError:
+                decrypted_message += letter
+        return decrypted_message
+
+
+    def create_kw_list(self, keyword):
+        """Prepares keyword string for processing by encrypt() and decrypt().
+
+        This is a helper function  that converts keyword from a string to a 
+        list so that keyword can be utilized more readily by the encrypt and 
+        decrypt methods.
+        """
+
+        keyword = keyword.upper()
+        self.keyword_list = []
+        for item in keyword:
+            self.keyword_list.append(item)
+
+
+    def create_kw_alphabet(self):
+        """Creates translation key for encrypting and decrypting messages.
+
+        This is a helper function that creates the alphabet used to translate 
+        messages using the Keyword algorithm.
+        """
+
+        kw_alphabet = copy.deepcopy(self.normal_alphabet)
+        for item in self.keyword_list:
+            for letter in kw_alphabet:
+                if item == letter:
+                    kw_alphabet.remove(letter)
+        kw_alphabet = self.keyword_list + kw_alphabet
+        return kw_alphabet
 
 
 if __name__ == '__main__':
-	lobo = Keyword()
-	print(lobo.encrypt("knowledge is power", "kryptos"))
-	print(lobo.decrypt("dghvetpst bm ihvtl", "kryptos"))
-	print(lobo.normal_alphabet)
-	print(lobo.keyword_alphabet)
+    lobo = Keyword()
+    print(lobo.encrypt("knowledge is power", "kryptos"))
+    print(lobo.decrypt("dghvetpst bm ihvtl", "kryptos"))
+    print(lobo.normal_alphabet)
+    print(lobo.keyword_alphabet)
--- atbash.py
+++ atbash.py
@@ -3,86 +3,86 @@ import string
 
 class Atbash(Cipher):
 
-	def __init__(self):
-		"""Initializes the Atbash cipher class.
+    def __init__(self):
+        """Initializes the Atbash cipher class.
 
-		The Atbash cipher class initializes itself with the a forward and  
-		backward uppercase ASCII alphabet as the alphabet to be used when 
-		encrypting and decrypting messages.
-		"""
+        The Atbash cipher class initializes itself with the a forward and  
+        backward uppercase ASCII alphabet as the alphabet to be used when 
+        encrypting and decrypting messages.
+        """
 
-		self.alphabet = string.ascii_uppercase
-		self.forward_list = self.create_forward_alpha_list(self.alphabet)
-		self.backward_list = self.create_backward_alpha_list(self.alphabet)
-		self.encrypt_key = dict(zip(self.forward_list, self.backward_list))
-		self.decrypt_key = dict(zip(self.forward_list, self.backward_list))
-		
+        self.alphabet = string.ascii_uppercase
+        self.forward_list = self.create_forward_alpha_list(self.alphabet)
+        self.backward_list = self.create_backward_alpha_list(self.alphabet)
+        self.encrypt_key = dict(zip(self.forward_list, self.backward_list))
+        self.decrypt_key = dict(zip(self.forward_list, self.backward_list))
+        
 
-	def encrypt(self, text):
-		"""Encrypts messages using the Atbash algorithm.
+    def encrypt(self, text):
+        """Encrypts messages using the Atbash algorithm.
 
-		text is the message to be encrypted. 
-		"""
+        text is the message to be encrypted. 
+        """
 
-		text = text.upper()
-		encrypted_message = ""
-		for item in text:
-			try:
-				encrypted_message += self.encrypt_key[item]
-			except KeyError:
-				encrypted_message += item
-		return encrypted_message
-
-
-	def decrypt(self, text):
-		"""Decrypts messages encrypted by the Atbash algorithm.
-
-		text is the message to be decrypted. 
-		"""
-
-		text = text.upper()
-		decrypted_message = ""
-		for item in text:
-			try:
-				decrypted_message += self.decrypt_key[item]
-			except KeyError:
-				decrypted_message += item
-		return decrypted_message
+        text = text.upper()
+        encrypted_message = ""
+        for item in text:
+            try:
+                encrypted_message += self.encrypt_key[item]
+            except KeyError:
+                encrypted_message += item
+        return encrypted_message
 
 
-	def create_backward_alpha_list(self, alphabet):
-		"""Creates alphabet to be used in __init__ method
+    def decrypt(self, text):
+        """Decrypts messages encrypted by the Atbash algorithm.
 
-		This is a helper function that creates an element of the key to be used
-		to encrypt and decrypt messages. 
-		"""
+        text is the message to be decrypted. 
+        """
 
-		backward_list = []
-		for item in alphabet[::-1]:
-			backward_list.append(item)
-		return backward_list
-
-	def create_forward_alpha_list(self, alphabet):
-		"""Creates alphabet to be used in __init__ method
-
-		This is a helper function that creates an element of the key to be used
-		to encrypt and decrypt messages. 
-		"""
+        text = text.upper()
+        decrypted_message = ""
+        for item in text:
+            try:
+                decrypted_message += self.decrypt_key[item]
+            except KeyError:
+                decrypted_message += item
+        return decrypted_message
 
 
-		forward_list = []
-		for item in alphabet:
-			forward_list.append(item)
-		return forward_list
+    def create_backward_alpha_list(self, alphabet):
+        """Creates alphabet to be used in __init__ method
+
+        This is a helper function that creates an element of the key to be used
+        to encrypt and decrypt messages. 
+        """
+
+        backward_list = []
+        for item in alphabet[::-1]:
+            backward_list.append(item)
+        return backward_list
+
+    def create_forward_alpha_list(self, alphabet):
+        """Creates alphabet to be used in __init__ method
+
+        This is a helper function that creates an element of the key to be used
+        to encrypt and decrypt messages. 
+        """
+
+
+        forward_list = []
+        for item in alphabet:
+            forward_list.append(item)
+        return forward_list
 
 
 if __name__ == '__main__':
-	atbash = Atbash()
-	print(atbash.forward_list)
-	print(atbash.backward_list)
-	print(atbash.ecrypt_key)
-	print(atbash.decrypt_key)
-	message = "ab  cd!"
-	print(atbash.encrypt(message))
-	secret = atbash.encrypt(message)
-	print(atbash.decrypt(secret))
+    atbash = Atbash()
+    print(atbash.forward_list)
+    print(atbash.backward_list)
+    print(atbash.ecrypt_key)
+    print(atbash.decrypt_key)
+    message = "ab  cd!"
+    print(atbash.encrypt(message))
+    secret = atbash.encrypt(message)
+    print(atbash.decrypt(secret))
