8301c750304b5e4ecd6da7deffdc94cce1b3d15d
Brian Peterson <brianpeterson28@gmail.com>
2017-12-26 19:47:24 -0600

    PEP 8 Edits
    Used pycodestyle to eliminate all PEP 8 offenses in Keyword.py

--- Keyword.py
+++ Keyword.py
@@ -2,12 +2,13 @@ from ciphers import Cipher
 import copy
 import string
 
+
 class Keyword(Cipher):
     def __init__(self):
         """Initializes the Keyword cipher class.
 
-        The Keyword cipher class initializes itself with the uppercase ASCII 
-        alphabet as the alphabet to be used when encrypting and decrypting 
+        The Keyword cipher class initializes itself with the uppercase ASCII
+        alphabet as the alphabet to be used when encrypting and decrypting
         messages.
         """
 
@@ -15,19 +16,18 @@ class Keyword(Cipher):
         for item in string.ascii_uppercase:
             self.normal_alphabet.append(item)
 
-
     def encrypt(self, text, keyword):
         """Encrypts messages using the Keyword algorithm.
 
-        text is the message to be encrypted. 
-        keyword is the keyword used to shift the alphabet. 
+        text is the message to be encrypted.
+        keyword is the keyword used to shift the alphabet.
         """
 
         text = text.upper()
         encrypted_message = ""
         self.create_kw_list(keyword)
         self.keyword_alphabet = self.create_kw_alphabet()
-        self.encrypt_key = dict(zip(self.normal_alphabet, 
+        self.encrypt_key = dict(zip(self.normal_alphabet,
                                     self.keyword_alphabet))
         for letter in text:
             try:
@@ -36,19 +36,18 @@ class Keyword(Cipher):
                 encrypted_message += letter
         return encrypted_message
 
-
     def decrypt(self, text, keyword):
-        """Decrypts messages encrypted by the Keyword algorithm. 
+        """Decrypts messages encrypted by the Keyword algorithm.
 
         text is the message to be decrypted.
-        keyword is the keyword that was originally used to encrypt the message. 
+        keyword is the keyword that was originally used to encrypt the message.
         """
 
         text = text.upper()
         decrypted_message = ""
         self.create_kw_list(keyword)
         self.keyword_alphabet = self.create_kw_alphabet()
-        self.decrypt_key = dict(zip(self.keyword_alphabet, 
+        self.decrypt_key = dict(zip(self.keyword_alphabet,
                                     self.normal_alphabet))
         for letter in text:
             try:
@@ -57,12 +56,11 @@ class Keyword(Cipher):
                 decrypted_message += letter
         return decrypted_message
 
-
     def create_kw_list(self, keyword):
         """Prepares keyword string for processing by encrypt() and decrypt().
 
-        This is a helper function  that converts keyword from a string to a 
-        list so that keyword can be utilized more readily by the encrypt and 
+        This is a helper function  that converts keyword from a string to a
+        list so that keyword can be utilized more readily by the encrypt and
         decrypt methods.
         """
 
@@ -71,11 +69,10 @@ class Keyword(Cipher):
         for item in keyword:
             self.keyword_list.append(item)
 
-
     def create_kw_alphabet(self):
         """Creates translation key for encrypting and decrypting messages.
 
-        This is a helper function that creates the alphabet used to translate 
+        This is a helper function that creates the alphabet used to translate
         messages using the Keyword algorithm.
         """
 
